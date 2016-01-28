421ef4fefc4598beb06493c137d260986a5bf8ae
aking <aking@DESKTOP-CLME1L4>
2016-01-28 10:55:02 +0000

    new

--- src/abhisheksam/Main.java
+++ src/abhisheksam/Main.java
@@ -3,23 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package abhishek.looselycoupled;
+package abhisheksam;
+
+import abhisheksam.tightlycoupled.Evidence;
+import abhisheksam.tightlycoupled.EvidenceBox;
+import abhisheksam.tightlycoupled.Policeman;
 
 /**
  *
  * @author aking
  */
-public class MainTest {
+public class Main {
 
-    public MainTest() {
-    }
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-         Evidence e1 = new Evidence("CCTV Footage");
+        // TODO code application logic here
+        Evidence e1 = new Evidence("CCTV Footage");
         Evidence e2 = new Evidence("DNA");
         EvidenceBox box = new EvidenceBox("S.Avery's  case", "2005000381");
         box.addEvidence(e1);box.addEvidence(e2);
         Policeman JLenk = new Policeman("J.Lenk");
         JLenk.addBoxes(box);
         JLenk.getAllPieceEvidence("2005000381", "DNA");
+        
     }
+
 }
